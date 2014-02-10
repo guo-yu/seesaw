1f259d55e3f7abd79389f891b7731b42d02c192b
Guo Yu <i@guoyu.me>
2014-02-10 20:35:30 +0800

    0.0.6: ensure body valid, shipping to 0.0.6

--- seesaw.js
+++ seesaw.js
@@ -4,19 +4,29 @@ var url = require('url'),
     express = require('express'),
     request = require('request');
 
+var ensureBody = function(body) {
+    if (typeof(body) !== 'object') return body;
+    try {
+        return JSON.stringify(body);
+    } catch (err) {
+        return body.toString();
+    }
+}
+
 var redirect = function(mockurl) {
     return function(req, res, next) {
         var target = {};
+        var isForm = req.headers['content-type'] === 'application/x-www-form-urlencoded' && req.method === 'POST';
         target.method = req.method;
         target.headers = req.headers;
         target.url = url.resolve(mockurl, req.url);
         target.query = req.query;
-        target.body = req.body;
-        target.files = req.files;
-        if (target.method === 'POST') target.form = target.body;
+        if (req.files) target.files = req.files;
+        if (!isForm && req.method !== 'GET') target.body = ensureBody(req.body);
+        if (isForm) target.form = req.body;
         request(target, function(err, response, body) {
             if (err) return res.send(err);
-            if (res.headers['content-type'] == 'application/json') return res.json(JSON.parse(body));                
+            if (response.headers['content-type'] == 'application/json') return res.json(JSON.parse(body));
             return res.send(body);
         });
     }
